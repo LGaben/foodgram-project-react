0e35c29608edb7a8eb16b8ff531f13d57528c1a7
Иван Филин <zobiemc@gmail.com>
2024-02-15 03:33:44 +0300

    Надеюсь

--- backend/foodgarm/api/urls.py
+++ backend/foodgarm/api/urls.py
@@ -1,6 +1,4 @@
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
@@ -29,7 +27,3 @@ urlpatterns = [
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
